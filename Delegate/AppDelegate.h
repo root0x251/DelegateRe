00f61c7adda947ad3b6dc04b4b198d748cebb740
Slava <rtootik@gmail.com>
2017-02-27 19:24:48 +0300

    Initial Commit

---
+++ Delegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Delegate
+//
+//  Created by Slava on 27.02.17.
+//  Copyright © 2017 Slava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Delegate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Delegate
+//
+//  Created by Slava on 27.02.17.
+//  Copyright © 2017 Slava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
